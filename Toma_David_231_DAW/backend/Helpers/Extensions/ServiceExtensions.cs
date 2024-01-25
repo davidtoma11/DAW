a0f63215b16046369a5de425b4f44e823d5d5653
Toma David <120136460+davidtoma11@users.noreply.github.com>
2024-01-25 17:03:13 +0200

    Add files via upload

---
+++ Toma_David_231_DAW/backend/Helpers/Extensions/ServiceExtensions.cs
@@ -0,0 +1,48 @@
+﻿using backend.Helpers.Seeders;
+using backend.Repositories.AutorRepository;
+using backend.Repositories.CarteAutorRepository;
+using backend.Repositories.CarteRepository;
+using backend.Repositories.EdituraRepository;
+using backend.Repositories.TestRepository;
+using backend.Services.AutorService;
+using backend.Services.CarteAutorService;
+using backend.Services.CarteService;
+using backend.Services.EdituraService;
+using backend.Services.TestService;
+
+namespace backend.Helpers.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddTransient<ITestRepository, TestRepository>();
+
+
+        services.AddTransient<IAutorRepository, AutorRepository>();
+        services.AddTransient<IEdituraRepository, EdituraRepository>();
+        services.AddTransient<ICarteRepository, CarteRepository>();
+        services.AddTransient<ICarteAutorRepository, CarteAutorRepository>();
+        return services;
+    }
+
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddTransient<ITestService, TestService>();
+
+
+        services.AddTransient<IAutorService, AutorService>();
+        services.AddTransient<IEdituraService, EdituraService>();
+        services.AddTransient<ICarteService, CarteService>();
+        services.AddTransient<ICarteAutorService, CarteAutorSerivce>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddSeeders(this IServiceCollection services)
+    {
+        services.AddTransient<TestSeeder>();
+    
+        return services;
+    }
+}
---
+++ Toma_David_231_DAW/backend/Services/EdituraService/EdituraService.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using backend.Models;
+using backend.Models.DTOs;
+using backend.Repositories.EdituraRepository;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace backend.Services.EdituraService
+{
+    public class EdituraService : IEdituraService
+    {
+        private readonly IEdituraRepository _edituraRepository;
+        private readonly IMapper _mapper;
+
+        public EdituraService(IEdituraRepository edituraRepository, IMapper mapper)
+        {
+            _edituraRepository = edituraRepository ?? throw new ArgumentNullException(nameof(edituraRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public async Task CreateEditura(EdituraDTO editura)
+        {
+            var edituraEntity = _mapper.Map<Editura>(editura);
+            await _edituraRepository.CreateAsync(edituraEntity);
+            await _edituraRepository.SaveAsync();
+        }
+
+        public async Task<List<Editura>> GetAll()
+        {
+            return await _edituraRepository.GetAllAsync();
+        }
+    }
+}
+
